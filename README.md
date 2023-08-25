2dbc53760de599e134fecdbcd209547f04bbc574
Lucas Ferraro <ferraro.luc@outlook.com>
2023-08-25 11:05:49 -0300

    terraform

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# educacionit-terraform
+# Educación IT - Terraform
+
+## Introducción
+
+El siguiente proyecto propone levantar una máquina virtual en la nube de AWS, y conectarse a la misma a través de una IP pública y utilizando el protocolo SSH, que utilizará un juego de llaves público-privado.
+
+## Credenciales de AWS
+
+Antes que nada, para correr la automatización se requieren unas credenciales `ACCESS_KEY` y `SECRET_KEY` válidas. Las mismas se pueden insertar en la configuración del "provider" dentro del archivo `provider.tf` (la parte comentada) o bien, configurar con AWS-CLI, a través del comando `aws configure`.
+
+## Claves Público-Privado
+
+Ahora, corresponde generar un juego de llaves público-privado con el comando `ssh-keygen -f terraform-ec2-key`, dentro de la carpeta del proyecto de Terraform.
+
+## Ejecutar la automatización
+
+Levantar la infraestructura con el comando `terraform init` y luego aplicar automatización con `terraform apply`.
+
+## Conexión final
+
+Cuando termine de correr la automatización, Terraform devolverá la IP pública del servidor en forma de salida, y luego se debe establecer una conexión a la máquina virtual con el comando `ssh -i terraform-ec2-key ubuntu@XXX.XXX.XXX.XXX`, forzando la utilización de la llave privada.
---
+++ main.tf
@@ -0,0 +1,84 @@
+resource "aws_vpc" "lab-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "lab-subnet" {
+  cidr_block              = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.lab-vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-2a"
+}
+
+resource "aws_internet_gateway" "lab-igw" {
+  vpc_id = aws_vpc.lab-vpc.id
+}
+
+resource "aws_route_table" "lab-rt" {
+  vpc_id = aws_vpc.lab-vpc.id
+  
+  route {
+    cidr_block = "0.0.0.0/0" 
+    gateway_id = aws_internet_gateway.lab-igw.id 
+  }
+}
+
+resource "aws_route_table_association" "lab-rta"{
+    subnet_id      = aws_subnet.lab-subnet.id
+    route_table_id = aws_route_table.lab-rt.id
+}
+
+resource "aws_security_group" "lab-sg-ssh" {
+  name   = "allow_ssh"
+  vpc_id = aws_vpc.lab-vpc.id 
+
+  ingress {
+    description      = "allow ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_key_pair" "lab-key" {
+	key_name   = "terraform_ec2_key"
+	public_key = "${file("terraform-ec2-key.pub")}"
+}
+
+resource "aws_instance" "lab-vm" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.lab-key.key_name
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.lab-subnet.id
+  security_groups             = [aws_security_group.lab-sg-ssh.id]
+}
+
+output "instance_ip" {
+  description = "The public ip for ssh access"
+  value       = aws_instance.lab-vm.public_ip
+}
